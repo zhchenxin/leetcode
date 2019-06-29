cc60f10108c0de7f77881bdc8241673161f7a88b
chenxin <1577838065@qq.com>
2019-06-29 18:01:55 +0800

    027

---
+++ src/main/java/xyz/zhchenxin/leetcode/_027/Main.java
@@ -0,0 +1,29 @@
+package xyz.zhchenxin.leetcode._027;
+
+
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        if (3 != (new Solution()).removeElement(new int[]{0, 0, 1, 1, 1, 2, 2, 3, 3, 4})) {
+            throw new Exception("异常");
+        }
+    }
+
+}
+
+
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        if (nums.length == 0) return 0;
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] == val) {
+                continue;
+            }
+            nums[i+1] = nums[j];
+            i++;
+        }
+        return i+1;
+    }
+}
